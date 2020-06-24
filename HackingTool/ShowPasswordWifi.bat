244928b7745d7ec347e79b9cf612115c2b8c6bba
Pongsapat_Kamklang <fluk303fluk@gmail.com>
2020-06-24 23:55:42 +0700

    เปลี่ยนชื่อ

---
+++ HackingTool/ShowPasswordWifi.bat
@@ -0,0 +1,13 @@
+@echo off
+title Show Password Wifi
+:S
+cls
+netsh wlan show profiles
+echo ----------------------------------
+echo           Show Password 
+echo ----------------------------------
+set /p "NAME=User Name is >"
+netsh wlan show profiles %NAME% key=clear
+set /p "Back=Back Y/N > "
+if "%Back%"=="N" goto S
+lobby.bat
